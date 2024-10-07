18e9975e115b96dc6c4a2876c4e4453f9ce9294f
Richard Rauser <richard@raaza.net>
2024-10-07 20:25:12 +0300

    making panel min width 300px, upping max alert count system to 1000,

--- components/panel.tsx
+++ components/panel.tsx
@@ -1,5 +1,7 @@
 export const Panel = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="bg-default-300 min-width-300 rounded p-4">{children}</div>
+    <div className="panel bg-default-300 min-width-300 rounded p-4">
+      {children}
+    </div>
   );
 };
--- lib/consts.ts
+++ lib/consts.ts
@@ -1,2 +1,2 @@
 export const MAX_ALERT_COUNT_USER = 3;
-export const MAX_ALERT_COUNT_SYSTEM = 25;
+export const MAX_ALERT_COUNT_SYSTEM = 1000;
